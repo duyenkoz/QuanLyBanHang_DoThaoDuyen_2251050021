d79a395aa9f203e555fce4dd019b194598f52383
duyenkoz <dienyonce@gmail.com>
2025-08-12 23:14:18 +0700

    update-duyen

--- web/models/user.py
+++ web/models/user.py
@@ -1,6 +1,8 @@
+import hashlib
 from sqlalchemy import Column, Integer, String, Enum, TIMESTAMP, func
 from sqlalchemy.ext.declarative import declarative_base
 import enum
+from web import db
 
 Base = declarative_base()
 
@@ -20,3 +22,14 @@ class User(Base):
     Email = Column(String(100))
     Address = Column(String(255))
     Created_at = Column(TIMESTAMP, server_default=func.now())
+
+def seed_data():
+    if not User.query.first():
+        admin = User(
+            name="Admin",
+            username="admin",
+            password=str(hashlib.md5('admin'.encode('utf-8')).hexdigest()),
+            user_role=UserRole.ADMIN
+        )
+        db.session.add(admin)
+        db.session.commit()
